f168f6a1dbcc3ae463384af927659d31c5d6993e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2018-07-02 22:10:22 -0400

    Updated initial page UI, updated script to get guidelines from server

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+const search = function () {
+    // Getting search term
+    let searchTerm = $('#search-input').val();
+
+    // Get concatenated data with only search text
+    searchData = getSearchData(searchTerm);
+}
+
+const getSearchData = function () {
+    // Return modified JSON, with only data that shows up in search terms
+}
+
+const populateGuidelineView = function (guideline_data) {
+    // Populate page with guideline data
+}
+
+
+// Get guidelines from server 
+guideline_get = $.get('ADA2018Guidelines.json', function (response) {
+    const full_guidelines = response
+    console.log(full_guidelines)
+});
